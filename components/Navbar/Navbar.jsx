98ceb221d44e3fb2c1457136f2caa558c97fb05f
Amrita Gupta <amamrita0818@gmail.com>
2024-02-06 17:36:34 +0530

    import images and created button.jsx and index.js

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React , {useState, useEffectfrom} from 'react'
+import Image from "next/image";
+import Link from "next/link";
+
+//import icons
+import {MdNotifications} from 'react-icons/md';
+import {BsSearch } from "react-icons/bs";
+import {CgMenuLeft , CgMenuRight} from "react-icons/cg";
+
+
+//internal import
+import Style from './Navbar.module.css';
+import {Discover, HelpCenter, Notification, Profile, Sidebar} from './index'
+import {Button} from "../componentsindex";
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
